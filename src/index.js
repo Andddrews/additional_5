64b6bcb6ad32f695f9251a15d3e6a95d6d386dc2
Andddrews <Andddrews@gmail.com>
2018-09-23 10:36:34 +0300

    add5

--- src/index.js
+++ src/index.js
@@ -1,3 +1,75 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+ bracketsConfig = {'(': ')', '{': '}', '[': ']', '|': '|'};
+  
+  var arr = str.split('');
+
+  for (i=0; i<=arr.length; i++) {
+      var a = -1;
+      var b = -1;
+      var a = arr.indexOf( "(", 0 );
+      var b = arr.indexOf( ")", 0 );
+      if (a >= 0 && b>=0) {
+        arr.splice (a, 1);
+        arr.splice (b, 1);
+      } else {
+        return false;
+      }
+      if (a===-1 || b===-1) {
+        return true;
+      }
+  }
+var arr = str.split('');
+  for (i=0; i<=arr.length; i++) {
+    var a = -1;
+    var b = -1;
+    var a = arr.indexOf( "{", 0 );
+    var b = arr.indexOf( "}", 0 );
+    if (a >= 0 && b>=0) {
+      arr.splice (a, 1);
+      arr.splice (b, 1);
+    } else {
+      return false;
+    }
+    if (a===-1 || b===-1) {
+      return true;
+    }
+    
+  }
+  var arr = str.split('');
+  for (i=0; i<=arr.length; i++) {
+    var a = -1;
+    var b = -1;
+    var a = arr.indexOf( "[", 0 );
+    var b = arr.indexOf( "]", 0 );
+    if (a >= 0 && b>=0) {
+      arr.splice (a, 1);
+      arr.splice (b, 1);
+    } else {
+      return false;
+    }
+    if (a===-1 || b===-1) {
+      return true;
+    }
 }
+
+var arr = str.split('');
+for (i=0; i<=arr.length; i++) {
+  var a = -1;
+  var b = -1;
+  var a = arr.indexOf( "|", 0 );
+  var b = arr.indexOf( "|", 0 );
+  if (a >= 0 && b>=0) {
+    arr.splice (a, 1);
+    arr.splice (b, 1);
+  } else {
+    return false;
+  }
+  if (a===-1 || b===-1) {
+    return true;
+  }
+}
+
+}
+
+
+
